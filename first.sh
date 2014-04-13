a02214e22c1114255eefdbbd9da931d15354031a
CrowdHailer <petersaxton@londonlayout.co.uk>
2014-04-13 20:43:29 +0100

    Script changed to install packages

--- first.sh
+++ first.sh
@@ -1,1 +1,7 @@
-echo 'Hadooken'
+packages='git xclip tree nautilus guake'
+
+echo 'This is the Almighty script for Machine rest.'
+echo "The packages to be installed are ${packages}"
+echo 'Please press enter to continue:'
+read
+sudo apt-get install $packages
