fb1677ac20242a3a9c5722655b714b7e10a355b1
Peter Saxton <peterhsaxton@gmail.com>
2015-06-01 13:07:24 +0100

    Delete first.sh

--- first.sh
+++
@@ -1,7 +0,0 @@
-packages='git xclip tree nautilus guake inkscape chromium-browser imagemagick curl'
-
-echo 'This is the Almighty script for Machine rest.'
-echo "The packages to be installed are ${packages}"
-echo 'Please press enter to continue:'
-read
-sudo apt-get install $packages
